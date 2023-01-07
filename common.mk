ed5362942de8fa0a25f9d028effd862cb1425f07
Orimitsu Serizawa <weave_light-00@yahoo.co.jp>
2023-01-07 15:34:30 +0900

    Organized Makefile

---
+++ common.mk
@@ -0,0 +1,24 @@
+VIVADO_DIR := /tools/Xilinx/Vivado/2022.2/bin
+XVLOG := $(VIVADO_DIR)/xvlog --incr
+XELAB := $(VIVADO_DIR)/xelab --incr
+XSIM  := $(VIVADO_DIR)/xsim
+
+COM_DIR = xsim.dir/work
+
+run : $(COM_DIR).test_bench
+	$(XSIM) test_bench --runall $(RUN_OPT)
+
+gui : $(COM_DIR).test_bench
+	$(XSIM) test_bench --gui
+
+$(COM_DIR)/dut.sdb : ../dut.sv
+	$(XVLOG) -L uvm --sv $<
+$(COM_DIR)/test_bench.sdb : ./test_bench.sv
+	$(XVLOG) -L uvm --sv $<
+
+$(COM_DIR).test_bench : $(COM_DIR)/dut.sdb $(COM_DIR)/test_bench.sdb
+	$(XELAB) -L uvm test_bench
+
+clean :
+	rm -rf *.log *.jou *.pb *.dir *.wdb *.vcd
+
